84bfb55ce4b4bbf3e5aa0c42c9fd467c602b4d19
Task Nagashige <task.19.xx@gmail.com>
2020-04-11 15:55:30 +0900

    フォームサンプルの作成

---
+++ step2/action.php
@@ -0,0 +1,2 @@
+こんにちは、<?php echo htmlspecialchars($_POST['name']); ?>さん。
+あなたは、<?php echo (int)$_POST['age']; ?> 歳です。
---
+++ step2/index.php
@@ -0,0 +1,5 @@
+<form action="action.php" method="post">
+    名前: <input type="text" name="name" />
+    年齢: <input type="text" name="age" />
+    <input type="submit" />
+</form>
